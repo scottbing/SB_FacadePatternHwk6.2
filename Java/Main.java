cc9104bad1b39668fedb5ddab553e538b07aabfb
Scott Bing <bingscott@gmail.com>
2020-02-21 20:25:17 -0700

    SB_FacadePatternHwk6.2
    SB_FacadePatternHwk6.2

---
+++ Java/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ShapeMaker shapeMaker = new ShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+}
